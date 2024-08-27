4bbc8e1beef3fdea04c842515edda4805fc109a9
Scott K. Anderson <skaclmbr@gmail.com>
2024-08-27 12:12:31 -0400

    added excel export function

--- web_scraper.py
+++ web_scraper.py
@@ -8,7 +8,14 @@ import json
 from pymongo.mongo_client import MongoClient
 # import mdbconn #stores database connection information
 import certifi
-from datetime import datetime, timedelta
+from datetime import datetime
+import os
+
+from openpyxl import Workbook #allows connecting to databases
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.formatting.rule import FormulaRule
+from openpyxl.styles import Font, PatternFill
+from openpyxl.styles.differential import DifferentialStyle
 
 #############################################################################
 ## LOOKUP DICTS
@@ -22,6 +29,168 @@ obs_status = {
 
 nl =" \n"
 fmt_dt = "%Y-%m-%d"
+today = datetime.now().strftime(fmt_dt)
+
+#############################################################################
+## OUTPUT EXCEL FILE
+dirpath = os.path.dirname(os.path.abspath(__file__))
+wb = Workbook()
+
+#field formats
+pctFields = [
+    "breedPctPossible",
+    "breedPctConfirmed",
+    "breedPctProbable"
+]
+bFields = [
+    "bbcgCoded",
+    "bbcgConfirmed",
+    "bbcgPossible",
+    "bbcgTotalEffortHours"
+]
+
+# Stats Sheet
+sc = {
+    "A" : "ID_EBD_NAME",
+    "B" : "tot",
+    "C" : "con",
+    "D" : "breedPctConfirmed",
+    "E" : "pos",
+    "F" : "breedPctPossible",
+    "G" : "pro",
+    "H" : "breedPctProbable",
+    "I" : "diurnal_hrs",
+    "J" : "nocturnal_hrs",
+    "K" : "num_checklists",
+    "L" : "num_atlasers",
+    "M" : "status",
+    "N" : "REGION",
+    "O" : "ECOREGION",
+    "P" : "COUNTY",
+    "Q" : "updateDate",
+    "R" : "ID_BLOCK_CODE",
+    "S" : "ID_NCBA_BLOCK",
+    "T" : "bbcgCoded",
+    "U" : "bbcgConfirmed",
+    "V" : "bbcgPossible",
+    "W" : "bbcgTotalEffortHrs"
+}
+colsStats = {}
+wsStats = wb.active
+wsStats.title = "Stats"
+maxStatCol = ""
+
+for k,v in sc.items():
+    wsStats[ k + "1"] = v
+    # flip these for lookup while saving data
+    colsStats[v] = k
+    maxStatCol = k
+
+
+#species sheet
+sppc = {
+    "A" : "ID_EBD_NAME",
+    "B" : "common_name",
+    "C" : "breeding_evidence",
+    "D" : "breeding_code",
+    "E" : "breeding_category",
+    "F" : "recent_location",
+    "G" : "observation_date",
+    "H" : "sampling_event_identifier",
+    "I" : "status",
+    "J" : "REGION",
+    "K" : "ECOREGION",
+    "L" : "COUNTY",
+    "M" : "updateDate",
+    "N" : "ID_BLOCK_CODE",
+    "O" : "ID_NCBA_BLOCK"
+}
+colsSpp = {}
+wsSpp = wb.create_sheet("Species")
+maxSppCol = ""
+for k,v in sppc.items():
+    wsSpp[ k + "1"] = v
+    colsSpp[v] = k
+    maxSppCol = k
+
+# Top Atlasers sheet
+tac = {
+    "A" : "ID_EBD_NAME",
+    "B" : "rank",
+    "C" : "observer",
+    "D" : "confirmed-species",
+    "E" : "status",
+    "F" : "REGION",
+    "G" : "ECOREGION",
+    "H" : "COUNTY",
+    "I" : "updateDate",
+    "J" : "ID_BLOCK_CODE",
+    "K" : "ID_NCBA_BLOCK"
+}
+colsTA = {}
+wsTopAtlasers = wb.create_sheet("Top Atlasers")
+maxTACol = ""
+
+for k,v in tac.items():
+    wsTopAtlasers[ k + "1"] = v
+    colsTA[v] = k
+    maxTACol = k
+
+# Recent Visits sheet
+rvc = {
+    "A" : "ID_EBD_NAME",
+    "B" : "observer",
+    "C" : "observation_date",
+    "D" : "checklist",
+    "E" : "observation_start_time",
+    "F" : "recent_location",
+    "G" : "num_spp",
+    "H" : "REGION",
+    "I" : "ECOREGION",
+    "J" : "COUNTY",
+    "K" : "updateDate",
+    "L" : "ID_BLOCK_CODE",
+    "M" : "ID_NCBA_BLOCK"
+}    
+colsRV = {}
+wsRecentVisits = wb.create_sheet("Recent Visits")
+maxRVCol = ""
+for k,v in rvc.items():
+    wsRecentVisits[ k + "1"] = v
+    colsRV[v] = k
+    maxRVCol = k
+
+
+#############################################################################
+## EXCEL FUNCTIONS
+style = TableStyleInfo(
+    name="TableStyleMedium9",
+    showFirstColumn=False,
+    showLastColumn=False,
+    showRowStripes=True,
+    showColumnStripes=False
+    )
+
+red_fill = PatternFill(bgColor="FF7979")
+green_fill = PatternFill(bgColor="92D050")
+pct_style = "0.0%"
+
+# dxRed = DifferentialStyle(font=Font(bold=True), fill=red_fill)
+# dxGreen = DifferentialStyle(fill=green_fill)
+
+# rulConRed = 
+
+# def addCondFormat(ws, range, formula):
+#     pass
+
+
+def createTable(dname, maxCol, currRow, style = style):
+    tbl = Table(
+        displayName = dname,
+        ref = "A1:" + maxCol + str(currRow)
+    )
+    tbl.tableStyleInfo = style
+    return tbl
 
 #############################################################################
 ## DATE FUNCTIONS
@@ -44,7 +213,6 @@ breeding2_start = breeding1_end + 1
 breeding2_end = breeding2_start + (breeding_length/3)
 breeding3_start = breeding2_end + 1
 
-today = datetime.now().strftime(fmt_dt)
 update_date = today
 
 def fmt_date(t):
@@ -128,13 +296,13 @@ def get_species(tbody):
             bird["breeding_evidence"] = obs[0].strip()
             bird["breeding_code"] = obs[1].replace(")","").strip()
             try:
-                bird["breeding_category"] = obs_status[bd["breeding_evidence"]]
+                bird["breeding_category"] = obs_status[bird["breeding_evidence"]]
             except:
                 bird["breeding_category"] = ""
 
         else:
             bird["breeding_evidence"] = obs.strip()
-            bird["breeding_category"] = ""
+            bird["breeding_category"] = "C1"
 
         # location
         bird["recent_location"] = r.find(
@@ -206,9 +374,11 @@ def parse_page(parsed_html):
             case _:
                 pass
             
-    bd["bbcgTotalEffortHrs"] = (
+    if (
         bd["diurnal_hrs"] >= minBlockBreedDiurnalHrs
-        )
+        ):
+        bd["bbcgTotalEffortHrs"] =  1
+    else: bd["bbcgTotalEffortHrs"] = 0
             
     #############################################################################
     # GET CHECKLISTS/ATLASERS
@@ -231,14 +401,19 @@ def parse_page(parsed_html):
         if h != "period":
             bd[h] = int(d.text)
 
-    bd["bbcgCoded"] = (
+    if (
         bd["tot"] >= minBlockBreedCoded
-    )
+    ):
+        bd["bbcgCoded"] = 1
+    else: bd["bbcgCoded"] = 0
+    
     try:
         bd["breedPctConfirmed"] = bd["con"]/bd["tot"]
-        bd["bbcgConfirmed"] = ( 
+        if ( 
             bd["breedPctConfirmed"] > minBlockBreedConfirmedPct
-            )
+            ):
+            bd["bbcgConfirmed"] = 1
+        else: 0
     except: pass
     try:  
         bd["breedPctProbable"] = bd["pro"]/bd["tot"]
@@ -246,9 +421,11 @@ def parse_page(parsed_html):
 
     try:
         bd["breedPctPossible"] = bd["pos"]/bd["tot"]
-        bd["bbcgPossible"] = ( 
+        if( 
             bd["breedPctPossible"] <= maxBlockBreedPossiblePct
-            )
+            ):
+            bd["bbcgPossible"] = 1
+        else: bd["bbcgPossible"] = 0
     except:
         pass
     #############################################################################
@@ -345,8 +522,16 @@ def parse_recent_visits(html):
     return rv
 
 def main():
-
     # loop through blocks, hit eBird webpage, and parse data
+
+    # Excel Row counters
+    # row counters
+    rowST = 2
+    rowTA = 2
+    rowSP = 2
+    rowRV = 2
+
+    # BLOCK COUNTER
     bcount = 0
     for b in pblocks.keys():
         ## add basic block data
@@ -374,38 +559,95 @@ def main():
 
         pd["recent_visits"] = prv
 
-        print("data parsed for block", b, nl,
+        print("data parsed for block", b)
+        print("saving data to excel")
+
+        # POPULATE EXCEL
+
+        for k,v in pd.items():
+            if k == "recent_visits":
+                for j in v:
+                    # loop through items in array
+                    for x, y in j.items():
+                        #loop through fields
+                        if x == "checklist":
+                            wsRecentVisits[colsRV[x]+str(rowRV)] = (
+                                '=HYPERLINK("https://ebird.org/atlasnc/checklist/' +
+                                y + '", "' + y + '")'
+                            )
+                            
+                        else:
+                            wsRecentVisits[colsRV[x]+str(rowRV)] = y
+
+                    
+                    #add stats cols
+                    for x,y in colsStats.items():
+                        if x in colsRV.keys():
+                            wsRecentVisits[colsRV[x] + str(rowRV)] = pd[x]
+
+                    rowRV += 1
+            elif k == "spp":
+                for j in v:
+                    # loop through items in array
+                    for x, y in j.items():
+                        #loop through fields
+                        if x == "sampling_event_identifier":
+                            wsSpp[colsSpp[x]+str(rowSP)] = (
+                                '=HYPERLINK("https://ebird.org/atlasnc/checklist/' +
+                                y + '", "' + y + '")'
+                            )
+                        else:
+                            wsSpp[colsSpp[x]+str(rowSP)] = y
+                                            
+                    #add stats cols
+                    for x,y in colsStats.items():
+                        if x in colsSpp.keys():
+                            wsSpp[colsSpp[x] + str(rowSP)] = pd[x]
+
+                    rowSP += 1
+            elif k == "top_atlasers":
+                for j in v:
+                    # loop through items in array
+                    for x, y in j.items():
+                        #loop through fields
+
+                        wsTopAtlasers[colsTA[x]+str(rowTA)] = y
+                       
+                    #add stats cols
+                    for x,y in colsStats.items():
+                        if x in colsTA.keys():
+                            wsTopAtlasers[colsTA[x] + str(rowTA)] = pd[x]
+
+                    rowTA += 1
+            else: #all other fields
+                try:
+                    wsStats[colsStats[k] + str(rowST)] = v
+                except: pass
+
+                if k in pctFields:
+                    _c = wsStats[colsStats[k]+str(rowST)]
+                    _c.number_format = "0.0%"
+                #if field in another sheet, add
+
+
+
+        #advance row for next block
+        rowST += 1              
+              
+        print(nl,
             "uploading to atlas cache"
             )
         
         # update mongodb ebird data
-        # ewd.update_one(
-        #     {
-        #         "_id" : b
-        #     },
-        #     {
-        #         "$set" : pd 
-        #     },
-        #     upsert = True
-        # )
-        blocksum.update_one(
-            {
-                "ID_BLOCK_CODE" : b
-            },
-            {
-                "$set": {
-                    "ebird_web_data": pd
-                }
-            }
-        )
 
-        # # update block collection with status
-        # blocks.update_one(
+        # blocksum.update_one(
         #     {
-        #         "_id" : b
+        #         "ID_BLOCK_CODE" : b
         #     },
         #     {
-        #         "STATUS" : pd["status"].title()
+        #         "$set": {
+        #             "ebird_web_data": pd
+        #         }
         #     }
         # )
 
@@ -414,8 +656,141 @@ def main():
         print(str(bcount), "blocks updated.", nl)
 
         # # TESTING
-        # if bcount > 0: break
-        
+        # # if bcount > 19: break
+
+    # SAVE WORKBOOK
+    # Add tables
+
+    # backup row counters
+    rowRV -= 1
+    rowSP -= 1
+    rowST -= 1
+    rowTA -= 1
+
+    wsStats.add_table(
+        createTable("blockstats", maxStatCol, rowST)
+    )
+
+    ## add conditional formatting
+    ## Pct Confirmed
+    pctConfCol = colsStats["breedPctConfirmed"]
+    condCell1 = pctConfCol + "2"
+    condRng = pctConfCol + "2:" + pctConfCol + str(rowST) 
+    wsStats.conditional_formatting.add(
+        condRng,
+        FormulaRule(
+            formula = [
+                condCell1 + '<' + str(minBlockBreedConfirmedPct)
+            ],
+            stopIfTrue = False,
+            fill = red_fill
+        )
+    )
+
+    wsStats.conditional_formatting.add(
+        condRng,
+        FormulaRule(
+            formula = [
+                condCell1 + ">=" + str(minBlockBreedConfirmedPct)
+            ],
+            stopIfTrue = True,
+            fill= green_fill
+        )
+    )
+    ## Pct Possible
+    pctPossCol = colsStats["breedPctPossible"] 
+    condCell1 = pctPossCol + "2"
+    condRng = pctPossCol + "2:" + pctPossCol + str(rowST) 
+    wsStats.conditional_formatting.add(
+        condRng,
+        FormulaRule(
+            formula = [
+                condCell1 + '<' + str(maxBlockBreedPossiblePct)
+            ],
+            stopIfTrue = False,
+            fill = green_fill
+        )
+    )
+
+    wsStats.conditional_formatting.add(
+        condRng,
+        FormulaRule(
+            formula = [
+                condCell1 + ">=" + str(maxBlockBreedPossiblePct)
+            ],
+            stopIfTrue = True,
+            fill= red_fill
+        )
+    )
+    
+    ## Coded
+    condCol = colsStats["tot"] 
+    condCell1 = condCol + "2"
+    condRng = condCol + "2:" + condCol + str(rowST) 
+    wsStats.conditional_formatting.add(
+        condRng,
+        FormulaRule(
+            formula = [
+                condCell1 + '>=' + str(minBlockBreedCoded)
+            ],
+            stopIfTrue = False,
+            fill = green_fill
+        )
+    )
+
+    wsStats.conditional_formatting.add(
+        condRng,
+        FormulaRule(
+            formula = [
+                condCell1 + "<" + str(minBlockBreedCoded)
+            ],
+            stopIfTrue = True,
+            fill= red_fill
+        )
+    )
+    
+    ## Diurnal Hours
+    condCol = colsStats["diurnal_hrs"] 
+    condCell1 = condCol + "2"
+    condRng = condCol + "2:" + condCol + str(rowST) 
+    wsStats.conditional_formatting.add(
+        condRng,
+        FormulaRule(
+            formula = [
+                condCell1 + '>=' + str(minBlockBreedDiurnalHrs)
+            ],
+            stopIfTrue = False,
+            fill = green_fill
+        )
+    )
+
+    wsStats.conditional_formatting.add(
+        condRng,
+        FormulaRule(
+            formula = [
+                condCell1 + "<" + str(minBlockBreedDiurnalHrs)
+            ],
+            stopIfTrue = True,
+            fill= red_fill
+        )
+    )
+
+    wsSpp.add_table(
+        createTable("species", maxSppCol, rowSP)
+    )
+    wsTopAtlasers.add_table(
+        createTable("topatlasers", maxTACol, rowTA)
+    )
+    wsRecentVisits.add_table(
+        createTable("recentvisits", maxRVCol, rowRV)
+    )
+
+    wb.save(
+        os.path.join(
+            dirpath,
+            update_date + " ebird block stats.xlsx"
+        )
+    )    
 
 if __name__=="__main__":
     main();
